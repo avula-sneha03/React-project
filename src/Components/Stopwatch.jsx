356e989ba43f8b78d70bf523a46e90d9ac25380c
avula-sneha03 <avulasneha123@gmail.com>
2024-12-06 11:53:22 +0530

    Added functionality

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import "./Stopwatch.css"
+import React, { useState} from "react";
 function Stopwatch() {
+    const [count, SetCount]=useState(0);
+    function handleclick(){
+        return SetCount(count+1);
+    }
+    
   return (
-    <div>Stopwatch</div>
+    <div className="container">
+        <h1>Count: {count}</h1>
+        <button id="btn" onClick={handleclick}>Click me</button>
+        
+    </div>
   )
 }
 
